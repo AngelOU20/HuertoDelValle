aec3e56f4ad36354226556d42f6ab11ec6a8e947
Renato Valer <valer9826@gmail.com>
2021-08-14 09:50:35 -0500

    Creación MVC --auth

---
+++ HuertoDelValle/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HuertoDelValle.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
