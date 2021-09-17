305c7d9a4c0766ef8032552bdb3f512c50e7f6be
Julio Ucharima <julio_ucharima@usmp.pe>
2021-09-17 15:29:10 -0500

    Tiempo del correo no confirmado

--- Startup.cs
+++ Startup.cs
@@ -40,6 +40,12 @@ namespace HuertoDelValle
             services.AddTransient<IEmailSender, EmailSender>();
             services.Configure<AuthMessageSenderOptions>(Configuration);
 
+            /*Tiempo de espera de inactividad del correo */
+            services.ConfigureApplicationCookie(o => {
+                o.ExpireTimeSpan = TimeSpan.FromDays(5);
+                o.SlidingExpiration = true;
+            });
+
             services.AddRazorPages();
             
             services.AddControllersWithViews();
