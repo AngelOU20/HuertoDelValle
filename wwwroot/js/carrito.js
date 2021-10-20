3b2cc188fd052525a187201b49ea5f18cc5117aa
Julio Ucharima <julio_ucharima@usmp.pe>
2021-10-20 12:08:43 -0500

    estilos

---
+++ wwwroot/js/carrito.js
@@ -0,0 +1,10 @@
+  /*Pop up && validación en tienda*/
+  function mostrar() {
+    div = document.getElementById('flotante');
+    div.style.display = '';
+    }
+
+    function cerrar() {
+        div = document.getElementById('flotante');
+        div.style.display = 'none';
+    }
